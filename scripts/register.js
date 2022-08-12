ece1b1250681e9c54e07d5b21ae70821f087fe0c
prd-cuong-vu <cuong.vu@paradox.ai>
2022-08-12 13:31:03 +0700

    register

--- scripts/register.js
+++ scripts/register.js
@@ -1,1 +1,45 @@
-'use strict'
+"use strict";
+
+const firstName = document.getElementById("input-firstname");
+const lastName = document.getElementById("input-lastname");
+const userName = document.getElementById("input-username");
+const password = document.getElementById("input-password");
+const confirmPassword = document.getElementById("input-password-confirm");
+
+const btnSubmit = document.getElementById("btn-submit");
+
+const users = getFromStore(KEYS.USER);
+
+const isValidUser = (user) => {
+  const isInputHasValue = [...document.querySelectorAll("input")].every(
+    (input) => input.value.length
+  );
+
+  const isUniqUsername = !users.find(
+    (storeUser) => storeUser.userName !== user.userName
+  );
+
+  const password = user.password;
+
+  return (
+    isInputHasValue && isUniqUsername && password === confirmPassword.value
+  );
+};
+
+const onSaveUser = () => {
+  const user = new User(
+    firstName.value,
+    lastName.value,
+    userName.value,
+    password.value
+  );
+  if (isValidUser(user)) {
+    users.push(user);
+    saveToStore(KEYS.USER, users);
+    alert("Dang ky thanh cong");
+  } else {
+    alert("Du lieu sai. Kiem tra lai du lieu");
+  }
+};
+
+btnSubmit.addEventListener("click", onSaveUser);
--- scripts/storage.js
+++ scripts/storage.js
@@ -1,1 +1,18 @@
-'use strict'
+"use strict";
+const KEYS = {
+  USER: "user",
+};
+
+const getFromStore = (key) => {
+  const strValue = localStorage.getItem(key);
+  if (strValue) {
+    return JSON.parse(strValue);
+  }
+  return [];
+};
+
+const saveToStore = (key, val) => {
+  const jsonValue = JSON.stringify(val);
+
+  localStorage.setItem(key, jsonValue);
+};
